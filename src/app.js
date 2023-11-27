1fe16cfeee6679daf577be032df54b1dee2fafed
Steve <diasm2@gmail.com>
2023-11-27 22:25:54 +0900

    handlebar example

--- src/app.js
+++ src/app.js
@@ -4,14 +4,34 @@ import UsersRouter from './routes/users.router.js';
 import cookieParser from 'cookie-parser';
 import logMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
+import { engine } from 'express-handlebars';
 
 const app = express();
-const PORT = 3334;
+const PORT = 3335;
 
 // app.use(logMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('public'));
+
+// app.engine('hbs', hbs({ extname: 'hbs' }));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.use(express.static('views/images'))
+app.set('views', './views');
+// app.use(express.static('public'));
+
+
+
+app.get('/', (req, res) => {
+  res.render('index', { layout: 'main' });
+})
+
+app.get('/getReserv', function(req, res, next) {
+  res.render('index', {layout:'getReserv'});
+});
+
+
+
 app.use('/api', [UsersRouter, ReservationRouter]);
 
 app.use(ErrorHandlingMiddleware);
