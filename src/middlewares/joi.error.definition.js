634e5e67d7cbb18fed52a5a97d15a548e4d8a4bb
Jhyun Y <140397466+yjhorion@users.noreply.github.com>
2023-11-29 21:51:54 +0900

    Update joi.error.definition.js

--- src/middlewares/joi.error.definition.js
+++ src/middlewares/joi.error.definition.js
@@ -7,8 +7,8 @@ const createSignUp = joi.object({
     'string.empty': 'nickname을 입력해주세요.',
     'string.alphanum': 'nickname에는 영어와 숫자만 작성해주세요.',
   }),
-  password: joi.string().alphanum().min(8).max(20).messages({
-    'string.min': 'password를 8글자 이상으로 작성해주세요.',
+  password: joi.string().alphanum().min(6).max(20).messages({
+    'string.min': 'password를 6글자 이상으로 작성해주세요.',
     'string.max': 'password를 20글자 이하으로 작성해주세요.',
     'string.empty': 'password를 입력해주세요.',
     'any.invalid': 'password에 nickname이 포함되면 안됩니다.',
@@ -23,8 +23,8 @@ const createSignIn = joi.object({
     'string.empty': 'nickname을 입력해주세요.',
     'string.alphanum': 'nickname에는 영어와 숫자만 작성해주세요.',
   }),
-  password: joi.string().alphanum().min(8).max(20).messages({
-    'string.min': 'password를 8글자 이상으로 작성해주세요.',
+  password: joi.string().alphanum().min(6).max(20).messages({
+    'string.min': 'password를 6글자 이상으로 작성해주세요.',
     'string.max': 'password를 20글자 이하으로 작성해주세요.',
     'string.empty': 'password를 입력해주세요.',
     'string.alphanum': 'password에는 영어와 숫자만 작성해주세요.',
